50b4a354c7c193d2a99337e11942fad068cd26a2
VsIG <Valentyn2202@gmail.com>
2020-11-22 23:18:20 +0200

    ✨: create class for storing data

---
+++ Async-Kursach/Async-Kursach/ConfigurationData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Kursach
+{
+	/// <summary>
+	/// Class for storing information from 
+	/// a csv or json file for using in other classes
+	/// </summary>
+	class ConfigurationData
+	{
+
+	}
+}
