1698a1da97e387ded20b06042e9691701c0fe268
VsIG <Valentyn2202@gmail.com>
2020-11-22 22:39:58 +0200

    ✨: add first choice

--- Async-Kursach/Async-Kursach/Program.cs
+++ Async-Kursach/Async-Kursach/Program.cs
@@ -10,6 +10,24 @@ namespace Async_Kursach
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hi, My name is Async Kursach and " +
+				"I will help You to have some fun!");
+			Console.WriteLine("Which one do You want to do?");
+			Console.WriteLine("1 for Translation and 2 for " +
+				"{insert Your functionality}");
+
+			string userChoice = Console.ReadLine();
+
+			if (userChoice=="1")
+			{
+				//Console.WriteLine("Which one do You want to do?");
+				Console.WriteLine("Enter Your sentence:");
+				string userSentence = Console.ReadLine();
+			}
+			else
+			{
+			Console.WriteLine("That's the wrong number");
+			}
 		}
 	}
 }
