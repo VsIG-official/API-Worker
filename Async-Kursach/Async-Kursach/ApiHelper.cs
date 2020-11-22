c128db78b345a33e4e94244b4c6e0987c227fda8
VsIG <Valentyn2202@gmail.com>
2020-11-22 23:34:19 +0200

    ✨: create method with initializing of cllient

--- Async-Kursach/Async-Kursach/ApiHelper.cs
+++ Async-Kursach/Async-Kursach/ApiHelper.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace Async_Kursach
 {
 	class ApiHelper
 	{
+		// Create static, 'cause We need one client per application
 		public static HttpClient ApiClient { get; set; } = new HttpClient();
 
 		public static void InitializeClient()
 		{
-
+			ApiClient = new HttpClient
+			{
+				BaseAddress = new Uri("")
+			};
+			ApiClient.DefaultRequestHeaders.Accept.Clear();
+			ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 		}
 	}
 }
