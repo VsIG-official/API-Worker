c02d6c116cb1de24b67d9971bcaac1a7f04d2f85
VsIG <Valentyn2202@gmail.com>
2020-11-22 23:12:23 +0200

    ✨: create class for api client

---
+++ Async-Kursach/Async-Kursach/ApiHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Async_Kursach
+{
+	class ApiHelper
+	{
+		public static HttpClient ApiClient { get; set; } = new HttpClient();
+
+		public static void InitializeClient()
+		{
+
+		}
+	}
+}
