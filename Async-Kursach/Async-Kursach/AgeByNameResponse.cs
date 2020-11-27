b47ab6db59afb46619e3e4c5cb435bc561595407
VsIG <Valentyn2202@gmail.com>
2020-11-27 12:57:48 +0200

    ✨: add some sorking api usage

---
+++ Async-Kursach/Async-Kursach/AgeByNameResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Kursach
+{
+	class AgeByNameResponse
+	{
+		public string Name{ get; set; }
+		public int Age{ get; set; }
+	}
+}
--- Async-Kursach/Async-Kursach/Program.cs
+++ Async-Kursach/Async-Kursach/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace Async_Kursach
 {
 	class Program
 	{
+		
 		static void Main()
 		{
-			ApiHelper.InitializeClient();
-
 			Console.WriteLine("Hi, My name is Async Kursach and " +
 				"I will help You to have some fun!");
 			Console.WriteLine("Which one do You want to do?");
@@ -19,14 +21,30 @@ namespace Async_Kursach
 			{
 				//Console.WriteLine("Which one do You want to do?");
 				Console.WriteLine("Enter Your name:");
-				string userSentence = Console.ReadLine();
-				AgeByName ageApi = new AgeByName();
-				
+				string userName = Console.ReadLine();
+
+				string url = $"https://api.agify.io/?name={ userName }";
+
+				HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+				HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+				string response;
+
+				using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+				{
+					response = streamReader.ReadToEnd();
+				}
+
+				AgeByNameResponse ageResponse = JsonConvert.DeserializeObject<AgeByNameResponse>(response);
+
+				Console.WriteLine("Your name is {0} and Your predicted age is {1}",ageResponse.Name,ageResponse.Age);
 			}
 			else
 			{
 				Console.WriteLine("That's the wrong number");
 			}
+			Console.ReadLine();
 		}
 	}
 }
