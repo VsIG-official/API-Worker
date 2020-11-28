192f5d19a39a488c73ea33e906fd3a015c9aa608
VsIG <Valentyn2202@gmail.com>
2020-11-28 17:21:03 +0200

    ✨: add files for API with nation

---
+++ Async-Kursach/Async-Kursach/NationByName.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Async_Kursach
+{
+	public class NationByName
+	{
+		const string defaultString = "Valentyn";
+		public static async Task<NationByNameModel> LoadValue(string text)
+		{
+			string url;
+			if (text != defaultString)
+			{
+				url = $"https://api.genderize.io/?name={ text } ";
+			}
+			else
+			{
+				url = $"https://api.genderize.io/?name={ defaultString }";
+			}
+
+			//open a call to a client
+			//or open a new request of ApiClient as await for response
+			using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+			{
+				if (response.IsSuccessStatusCode)
+				{
+					// there We will read the data
+					// convert json to ValueByNameModel with correct properties
+					NationByNameModel genderAndProbability = await response.Content.ReadAsAsync<NationByNameModel>();
+
+					return genderAndProbability;
+				}
+				else
+				{
+					throw new Exception(response.ReasonPhrase);
+				}
+			}
+			//there a response is closing and make sure, that Our calls are cleaned up
+		}
+	}
+}
+
---
+++ Async-Kursach/Async-Kursach/NationByNameModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Async_Kursach
+{
+	public class NationByNameModel
+	{
+		public string Gender { get; set; }
+		public float Probability { get; set; }
+	}
+}
