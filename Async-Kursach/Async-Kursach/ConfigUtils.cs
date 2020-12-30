7d0769421a7d84d66d7b884f6e22c4e14f2a84b8
VsIG <Valentyn2202@gmail.com>
2020-12-30 18:10:59 +0200

    ✨: add all methods [ConfigUtils]

--- Async-Kursach/Async-Kursach/ConfigUtils.cs
+++ Async-Kursach/Async-Kursach/ConfigUtils.cs
@@ -10,6 +10,34 @@ namespace Async_Kursach
 	// mechanism for accessing the data
 	class ConfigUtils
 	{
+        #region Fields
 
-	}
+        static ConfigData configData;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the number of seconds in a game
+        /// </summary>
+        public static int Greeting
+        {
+            get { return configData.Greeting; }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Initializes the configuration data by creating the ConfigurationData object 
+        /// </summary>
+        public static void Initialize()
+        {
+            configData = new ConfigData();
+        }
+
+        #endregion
+    }
 }
