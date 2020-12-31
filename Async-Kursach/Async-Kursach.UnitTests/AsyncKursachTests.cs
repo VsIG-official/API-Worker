fc1de76824be7ac81d48e39deafe939f448b16db
VsIG <Valentyn2202@gmail.com>
2020-12-31 22:35:42 +0200

    ✨: first test is working

--- Async-Kursach/Async-Kursach.UnitTests/AsyncKursachTests.cs
+++ Async-Kursach/Async-Kursach.UnitTests/AsyncKursachTests.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Async_Kursach.Fundamentals;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Async_Kursach.UnitTests
 {
@@ -7,16 +10,18 @@ namespace Async_Kursach.UnitTests
 	public class AsyncKursachTests
 	{
 		[TestMethod]
-		public void LoadNameAge_ShouldReturnAge()
+		public async Task LoadNameAge_ShouldReturnAgeAsync()
 		{
 			// Arrange
 			string name = "Maks";
 			int expected = 29;
+			ApiHelper.Initialize();
 
 			// Act
-			
+			int actual = await ApiMethods.LoadNameAge(name).ConfigureAwait(false);
 
 			// Assert
+			Assert.AreEqual(expected, actual);
 		}
 	}
 }
--- Async-Kursach/Async-Kursach/Program.cs
+++ Async-Kursach/Async-Kursach/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 
 namespace Async_Kursach.Fundamentals
 {
