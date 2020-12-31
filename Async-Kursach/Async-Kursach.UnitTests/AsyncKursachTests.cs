72e899d744010d11ed2ab156d30c04f1c7140879
VsIG <Valentyn2202@gmail.com>
2020-12-31 14:52:38 +0200

    ✨: add new class

---
+++ Async-Kursach/Async-Kursach.UnitTests/AsyncKursachTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Async_Kursach.UnitTests
+{
+	[TestClass]
+	public class AsyncKursachTests
+	{
+		[TestMethod]
+		public void LoadNameAge_ShouldReturnAge()
+		{
+			// Arrange
+			string name = "Maks";
+			int expected = 29;
+
+			// Act
+			//int actual = LoadNameAge
+
+			// Assert
+		}
+	}
+}
--- Async-Kursach/Async-Kursach/Program.cs
+++ Async-Kursach/Async-Kursach/Program.cs
@@ -69,122 +69,13 @@ namespace Async_Kursach
 		private static void CreateOperations()
 		{
 			operations["1"] = async () => { await
-				LoadNameInfo().ConfigureAwait(false); };
+				ApiMethods.LoadNameInfo().ConfigureAwait(false); };
 			operations["2"] = async () => { await
-				LoadActivities().ConfigureAwait(false); };
+				ApiMethods.LoadActivities().ConfigureAwait(false); };
 			operations["3"] = async () => { await
-				LoadJokes().ConfigureAwait(false); };
+				ApiMethods.LoadJokes().ConfigureAwait(false); };
 			operations["4"] = async () => { await
-				CallAllAPI().ConfigureAwait(false); };
-		}
-
-		/// <summary>
-		/// Calls all API.
-		/// </summary>
-		private static async Task CallAllAPI()
-		{
-			await LoadNameInfo().ConfigureAwait(false);
-			Console.WriteLine();
-			await LoadActivities().ConfigureAwait(false);
-			Console.WriteLine();
-			await LoadJokes().ConfigureAwait(false);
-		}
-
-		/// <summary>
-		/// Loads the name information from API
-		/// </summary>
-		/// <param name="name">Name of person</param>
-		private static async Task LoadNameInfo()
-		{
-			Console.WriteLine(ConfigUtils.EnterName);
-			string userName = Console.ReadLine();
-
-			int age = await LoadNameAge(userName).
-				ConfigureAwait(false);
-			string[] genderData = await LoadNameGender
-				(userName).ConfigureAwait(false);
-
-			Console.WriteLine($"Here some data for {userName} name: \n" +
-				$"Your predicted age is {age} \n" +
-				$"Your predicted gender is {genderData[0]} with " +
-				$"probability of {genderData[1]}");
-		}
-
-		/// <summary>
-		/// Loads the name age from API
-		/// </summary>
-		/// <param name="name">Name of person</param>
-		/// <returns> age </returns>
-		private static async Task<int> LoadNameAge(string name)
-		{
-			AgeByNameModel age = await AgeByName.
-				LoadValue(name).ConfigureAwait(false);
-
-			return age.Age;
-		}
-
-		/// <summary>
-		/// Loads the name gender from API
-		/// </summary>
-		/// <param name="name">Name of person</param>
-		/// <returns> array with data </returns>
-		private static async Task<string[]> LoadNameGender(string name)
-		{
-			string[] genderData = new string[2];
-			GenderByNameModel gender = await GenderByName.
-				LoadValue(name).ConfigureAwait(false);
-
-			genderData[0] = gender.Gender;
-			genderData[1] = gender.Probability.ToString();
-
-			return genderData;
-		}
-
-		/// <summary>
-		/// Loads the activities from API
-		/// </summary>
-		/// <returns> array with data </returns>
-		private static async Task<string[]> LoadActivities()
-		{
-			string[] activitiesData = new string[4];
-			ActivitiesModel activities = await Activities.
-				LoadValue().ConfigureAwait(false);
-
-			activitiesData[0] = activities.Activity;
-			activitiesData[1] = activities.Type;
-			activitiesData[2] = activities.Participants.ToString();
-			activitiesData[3] = activities.Price.ToString();
-
-			Console.WriteLine($"There is some interesting activity: \n" +
-				$"Activity - {activitiesData[0]} \n" +
-				$"Type - {activitiesData[1]} with \n" +
-				$"Participants - {activitiesData[2]} \n" +
-				$"Price - {activitiesData[3]}");
-
-			return activitiesData;
-		}
-
-		/// <summary>
-		/// Loads the jokes from API
-		/// </summary>
-		/// <returns> array with data </returns>
-		private static async Task<string[]> LoadJokes()
-		{
-			string[] jokesData = new string[2];
-			JokesModel jokes = await Jokes.LoadValue()
-				.ConfigureAwait(false);
-
-			jokesData[0] = jokes.Setup;
-			jokesData[1] = jokes.Punchline;
-
-			Console.WriteLine($"Here comes the joke: \n" +
-				$"{jokesData[0]}\n" +
-				$"...\n" +
-				$"...\n" +
-				$"...\n" +
-				$"{jokesData[1]}");
-
-			return jokesData;
+				ApiMethods.CallAllAPI().ConfigureAwait(false); };
 		}
 	}
 }
