dd35dd9ddf41c22c27b3eb23ae74df334949e5a8
VsIG <Valentyn2202@gmail.com>
2021-01-02 00:08:14 +0200

    ✨: add a lot of tests

--- Async-Kursach/Async-Kursach.UnitTests/AsyncKursachTests.cs
+++ Async-Kursach/Async-Kursach.UnitTests/AsyncKursachTests.cs
@@ -15,6 +15,7 @@ namespace Async_Kursach.UnitTests
 			// Arrange
 			string name = "Maks";
 			int expected = 30;
+
 			ApiHelper.Initialize();
 
 			// Act
@@ -31,6 +32,7 @@ namespace Async_Kursach.UnitTests
 			// Arrange
 			string name = "Maks";
 			int expected = 20;
+
 			ApiHelper.Initialize();
 
 			// Act
@@ -39,6 +41,120 @@ namespace Async_Kursach.UnitTests
 
 			// Assert
 			Assert.AreNotEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public async Task LoadNameGender_MaksGender_ReturnTrue()
+		{
+			// Arrange
+			string name = "Maks";
+			string expected = "male";
+
+			ApiHelper.Initialize();
+
+			// Act
+			string[] actual = await ApiMethods.LoadNameGender(name).
+				ConfigureAwait(false);
+
+			// Assert
+			Assert.AreEqual(expected, actual[0]);
+		}
+
+		[TestMethod]
+		public async Task LoadNameGender_MaksGender_ReturnFalse()
+		{
+			// Arrange
+			string name = "Maks";
+			string expected = "female";
+
+			ApiHelper.Initialize();
+
+			// Act
+			string[] actual = await ApiMethods.LoadNameGender(name).
+				ConfigureAwait(false);
+
+			// Assert
+			Assert.AreNotEqual(expected, actual[0]);
+		}
+
+		[TestMethod]
+		public async Task LoadNameGender_MaksProbability_ReturnTrue()
+		{
+			// Arrange
+			string name = "Maks";
+			string expected = "0,99";
+
+			ApiHelper.Initialize();
+
+			// Act
+			string[] actual = await ApiMethods.LoadNameGender(name).
+				ConfigureAwait(false);
+
+			// Assert
+			Assert.AreEqual(expected, actual[1]);
+		}
+
+		[TestMethod]
+		public async Task LoadNameGender_MaksProbability_ReturnFalse()
+		{
+			// Arrange
+			string name = "Maks";
+			string expected = "0,88";
+
+			ApiHelper.Initialize();
+
+			// Act
+			string[] actual = await ApiMethods.LoadNameGender(name).
+				ConfigureAwait(false);
+
+			// Assert
+			Assert.AreNotEqual(expected, actual[0]);
+		}
+
+		[TestMethod]
+		public async Task LoadNameInfo_Maks_ReturnTrue()
+		{
+			// Arrange
+			string name = "Maks";
+			int expectedAge = 30;
+			string[] expectedGender = { "male", "0,99" };
+
+			ApiHelper.Initialize();
+
+			// Act
+			int actualAge = await ApiMethods.LoadNameAge(name).
+				ConfigureAwait(false);
+
+			string[] actualGender = await ApiMethods.LoadNameGender(name).
+				ConfigureAwait(false);
+
+			// Assert
+			Assert.AreEqual(expectedAge, actualAge);
+			Assert.AreEqual(expectedGender[0], actualGender[0]);
+			Assert.AreEqual(expectedGender[1], actualGender[1]);
+		}
+
+		[TestMethod]
+		public async Task LoadNameInfo_Maks_ReturnFalse()
+		{
+			// Arrange
+			string name = "Maks";
+			int expectedAge = 20;
+			string[] expectedGender = { "female", "0,88" };
+
+			ApiHelper.Initialize();
+
+			// Act
+			int actualAge = await ApiMethods.LoadNameAge(name).
+				ConfigureAwait(false);
+
+			string[] actualGender = await ApiMethods.LoadNameGender(name).
+				ConfigureAwait(false);
+
+			// Assert
+			Assert.AreNotEqual(expectedAge, actualAge);
+			Assert.AreNotEqual(expectedGender[0], actualGender[0]);
+			Assert.AreNotEqual(expectedGender[1], actualGender[1]);
 		}
 	}
 }
